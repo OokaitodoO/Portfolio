1b50a557f49a5e48886730a58581cf3dfe845b46
mixer <m.kanjanawat@gmail.com>
2025-02-14 23:37:23 +0700

    Fix resume variable

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -1,27 +1,27 @@
 import React from 'react'
-import Style from './ResumeStyle.module.css'
+import './ResumeStyle.module.css'
 
 function Resume() {
   return (
     <section id='resume'>
-      <div className={Style.container}>
-        <div className={Style.title}>
+      <div className='container'>
+        <div className='title'>
           <h2>
             My <strong>Resume</strong>
           </h2>
         </div>
 
         {/*Education section*/}
-        <div className={className(Style.education, Style.group) }>
+        <div className='education group'>
           <h2>Education</h2>
-          <div className={Style.resume-item}>
-            <span className={Style.date}>
-              <span className={Style.icen-caldendar}></span>
+          <div className='resume-item'>
+            <span className='date'>
+              <span className='icen-caldendar'></span>
               Date
             </span>
             <h3>Master in Information Technology</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            <span className={Style.school}>Chiang Mai University</span>
+            <span className='school'>Chiang Mai University</span>
           </div>
         </div>
 
