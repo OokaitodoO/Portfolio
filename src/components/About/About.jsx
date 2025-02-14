54c9578b9e6e0dd49958eccf6e11b991cf8d13c6
mixer <m.kanjanawat@gmail.com>
2025-02-15 01:35:42 +0700

    Add resume section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './AboutStyle.css'
 
 function About() {
   return (
--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './ResumeStyle.module.css'
+import './ResumeStyle.css'
 
 function Resume() {
   return (
@@ -11,24 +11,73 @@ function Resume() {
           </h2>
         </div>
 
-        {/*Education section*/}
-        <div className='education group'>
-          <h2>Education</h2>
-          <div className='resume-item'>
-            <span className='date'>
-              <span className='icen-caldendar'></span>
-              Date
-            </span>
-            <h3>Master in Information Technology</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            <span className='school'>Chiang Mai University</span>
+        <div className='group'>
+          {/*Education section*/}
+          <div className='education'>
+            <h2>Education</h2>
+            <div className='resume-item'>
+              <span className='date'>
+                <span className='icon-caldendar'></span>
+                March 2013 - Present
+             </span>
+              <h3>Master in Information Technology</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              <span className='school'>Chiang Mai University</span>
+            </div>
+            <div className='resume-item'>
+              <span className='date'>
+                <span className='icon-caldendar'></span>
+                March 2013 - Present
+              </span>
+              <h3>Master in Information Technology</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              <span className='school'>Chiang Mai University</span>
+            </div>
+            <div className='resume-item'>
+              <span className='date'>
+                <span className='icon-caldendar'></span>
+                March 2013 - Present
+              </span>
+              <h3>Master in Information Technology</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              <span className='school'>Chiang Mai University</span>
+            </div>
           </div>
-        </div>
 
-        {/*Exprirence section*/}
-        <div className='exprirence group'>
+          {/*Exprirence section*/}
+          <div className='exprirence'>
+            <h2>Exprirence</h2>
+            <div className='resume-item'>
+              <span className='date'>
+                <span className='icon-caldendar'></span>
+                March 2013 - Present
+              </span>
+              <h3>Master in Information Technology</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              <span className='school'>Chiang Mai University</span>
+            </div>
+            <div className='resume-item'>
+              <span className='date'>
+                <span className='icon-caldendar'></span>
+                March 2013 - Present
+              </span>
+              <h3>Master in Information Technology</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              <span className='school'>Chiang Mai University</span>
+            </div>
+            <div className='resume-item'>
+              <span className='date'>
+                <span className='icon-caldendar'></span>
+                March 2013 - Present
+              </span>
+              <h3>Master in Information Technology</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              <span className='school'>Chiang Mai University</span>
+            </div>
+          </div>
 
         </div>
+        
       </div>
         
     </section>
