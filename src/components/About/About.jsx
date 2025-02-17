e034176c404eea4bfd1c23572f63f606b869032b
Kanjanawat <m.kanjanawat@gmail.com>
2025-02-17 20:55:52 +0700

    Section about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,7 +4,20 @@ import './AboutStyle.css'
 function About() {
   return (
     <section id='about'>
-        <p>About</p>
+        <div className='container'>
+          <div className='about-img'>
+            <img src="https://picsum.photos/600/400" alt="Sample Image" />
+          </div>
+          <div className='about-text'>
+            <h2>About <strong>Me</strong></h2>
+            <p className='about-lead'>
+              Sawaddee kub khun por khun mae pee nhong wan nee mhoo huau ja mar tum website keaw gub portfolio khong tua eang.
+            </p>
+            <p className='about-description'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
+        </div>
     </section>
   )
 }
